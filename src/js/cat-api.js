0a8333f5dc0897910bfe5ccedc14a0a455560cef
mattmalicki <mateusz.malicki96@gmail.com>
2023-08-13 10:45:40 +0200

    almost done

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -11,7 +11,7 @@ async function fetchBreeds() {
         return response.data;
       });
   } catch (error) {
-    console.log(error);
+    onError(error);
   }
 }
 
@@ -20,12 +20,30 @@ async function fetchCatByBreed(breedId) {
     return await axios
       .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
       .then(function (response) {
-        console.log(response);
         return response.data[0];
       });
   } catch (error) {
-    console.log(error);
+    onError(error);
   }
+}
+
+function onError(error) {
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.data);
+    console.log(error.response.status);
+    console.log(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+  }
+  console.log(error.config);
 }
 
 export { fetchBreeds, fetchCatByBreed };
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
 import { fetchCatByBreed, fetchBreeds } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const selectEl = document.querySelector('.breed-select');
+
 const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 const catsEl = document.querySelector('.cat-info');
+
+selectEl.classList.add('is-hidden');
+errorEl.classList.add('is-hidden');
 
 fetchBreeds()
   .then(function (response) {
@@ -13,19 +19,30 @@ fetchBreeds()
         `<option value="${cat.id}">${cat.name}</option>`
       );
     }
+    loaderEl.classList.add('is-hidden');
+    selectEl.classList.remove('is-hidden');
+    new SlimSelect({
+      select: '.breed-select',
+    });
   })
-  .catch(function (error) {
-    console.log(error);
+  .catch(function () {
+    errorEl.classList.remove('is-hidden');
   });
 
 selectEl.addEventListener('change', function (event) {
-  fetchCatByBreed(event.target.value).then(function (response) {
-    showCat(response);
-  });
+  catsEl.innerHTML = '';
+  loaderEl.classList.toggle('is-hidden');
+  fetchCatByBreed(event.target.value)
+    .then(function (response) {
+      loaderEl.classList.toggle('is-hidden');
+      showCat(response);
+    })
+    .catch(function () {
+      errorEl.classList.remove('is-hidden');
+    });
 });
 
 function showCat(cat) {
-  console.log(cat);
   const imgEl = `<img src="${cat.url}" width="300px" />`;
   const infoEl = `<div><h2>${cat.breeds[0].name}</h2><p>${cat.breeds[0].description}</p><p>${cat.breeds[0].temperament}</p></div>`;
   catsEl.insertAdjacentHTML('beforeend', imgEl);
